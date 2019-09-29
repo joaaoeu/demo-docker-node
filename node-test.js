22c50606443267f981df2428c424c674b5603026
joaaoeu <hello@joaao.eu>
2019-09-29 17:08:24 +0100

    Added node-test.js

---
+++ node-test.js
@@ -0,0 +1,17 @@
+const https = require('https');
+
+https
+  .get('https://jsonplaceholder.typicode.com/todos/1', response => {
+    let todo = '';
+
+    response.on('data', chunk => {
+      todo += chunk;
+    });
+
+    response.on('end', () => {
+      console.log(`The title is "${JSON.parse(todo).title}"`);
+    });
+  })
+  .on('error', error => {
+    console.error('Error: ' + error.message);
+  });
